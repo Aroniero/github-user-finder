7fdb97c58e19ffc18184cf3bff80a6201a14cfbd
Łukasz Godlewski <AronieroDYami@gmail.com>
2020-08-08 17:30:51 +0200

    Updates

---
+++ precache-manifest.6024988e9517124c9f5f00e8ab057cb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720fc5a687cfb6024d4cc6f670415d4b",
+    "url": "/github-user-finder/index.html"
+  },
+  {
+    "revision": "3ebdd8365288ead31232",
+    "url": "/github-user-finder/static/js/2.711fd9d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-user-finder/static/js/2.711fd9d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8b3108f3215fd7849a4",
+    "url": "/github-user-finder/static/js/main.6fef76b5.chunk.js"
+  },
+  {
+    "revision": "cd2b9deb4e05d8644b82",
+    "url": "/github-user-finder/static/js/runtime-main.af604fb4.js"
+  }
+]);
