55f50813ae87288e961cedbd8c947d61d18403e4
Arkadiusz Bachorski <quez2223@gmail.com>
2020-08-09 14:36:58 +0200

    Refactor Loading component
    * Remove unnecessary before and after
    * Replace position with better solution - flexbox
    * Handle delay with styled-components prop

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Bouncing, BouncingLoader, Container } from './Loading.style';
+import { Bouncing, InnerContainer, Container } from './Loading.style';
 
 const Loading = () => {
   return (
     <Container>
-      <BouncingLoader>
-        <Bouncing />
-      </BouncingLoader>
+      <InnerContainer>
+        <Bouncing delay={0} />
+        <Bouncing delay={0.2} />
+        <Bouncing delay={0.4} />
+      </InnerContainer>
     </Container>
   );
 };
--- src/components/Loading/Loading.style.js
+++ src/components/Loading/Loading.style.js
@@ -10,42 +10,22 @@ const bounce = keyframes`
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: ${({ theme }) => theme.colors.blue};
 `;
 
-export const BouncingLoader = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translateX(-50%);
-
-  ::before,
-  ::after {
-    display: inline-block;
-    width: 23px;
-    height: 23px;
-    background: white;
-    border-radius: 50%;
-    margin-bottom: -5px;
-
-    animation: 0.6s ${bounce} infinite alternate;
-    content: '';
-  }
-
-  ::after {
-    animation-delay: 0.4s;
-  }
+export const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
 
 export const Bouncing = styled.div`
-  display: inline-block;
   width: 23px;
   height: 23px;
   background: white;
   margin: 0 5px -5px;
   border-radius: 50%;
-
-  content: '';
-  animation: ${bounce} 0.6s infinite alternate;
-  animation-delay: 0.2s;
+  animation: ${bounce} 0.6s ${({ delay }) => delay}s infinite alternate;
 `;
